a9e7ceed7fe13a499895e21d95dca9f4fb858cf4
Leonardo Vechieti <leonardovechieti@outlook.com>
2025-03-19 14:54:33 -0300

    Feat | Rotas dinâmicas

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,26 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
+import 'package:meals/screens/categories_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final Category category;
 
   const CategoryItem({super.key, required this.category});
 
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return CategoriesMealsScreen(category: category);
+        },
+      ),
+    );
+     
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(15),
-      child: Text(category.title, style: TextStyle(fontSize: 15, fontFamily: 'RobotoCondensed', fontWeight: FontWeight.bold)),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.7),
-            category.color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      splashColor: Theme.of(context).colorScheme.secondary,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Text(category.title, style: TextStyle(fontSize: 15, fontFamily: 'RobotoCondensed', fontWeight: FontWeight.bold)),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.7),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
         ),
-        borderRadius: BorderRadius.circular(15),
       ),
     );
   }
---
+++ lib/screens/categories_meals_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+
+class CategoriesMealsScreen extends StatelessWidget {
+
+  final Category category;
+
+  const CategoriesMealsScreen({super.key, required this.category});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(category.title),
+      ),
+      body: Center(
+        child: Text('Receitas por Categoria'),
+      ),
+    );
+  }
+}
