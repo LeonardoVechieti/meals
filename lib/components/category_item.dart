9cfc33c18e5595eb514d7621d2fd27966f2529d0
Leonardo Vechieti <leonardovechieti@outlook.com>
2025-03-19 15:02:54 -0300

    Feat | Rotas por pushName e Argumentos

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -8,14 +8,18 @@ class CategoryItem extends StatelessWidget {
   const CategoryItem({super.key, required this.category});
 
   void _selectCategory(BuildContext context) {
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (_) {
-          return CategoriesMealsScreen(category: category);
-        },
-      ),
-    );
+    // Navigator.of(context).push(
+    //   MaterialPageRoute(
+    //     builder: (_) {
+    //       return CategoriesMealsScreen(category: category);
+    //     },
+    //   ),
+    // );
      
+     Navigator.of(context).pushNamed(
+       '/categories-meals',
+       arguments: category,
+     );
   }
 
   @override
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/screens/categories_screen.dart';
+import 'package:meals/screens/categories_meals_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,6 +16,10 @@ class MyApp extends StatelessWidget {
             .copyWith(secondary: Colors.amber),
       ),
       home: CategoriesScreen(),
+      routes: {
+        '/categories-meals': (ctx) => CategoriesMealsScreen(),
+        
+      },
     );
   }
 }
